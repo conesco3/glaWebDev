5287d98eeb01d55f0e8ad3f05f86078b2501a73f
Neil Boyd <conesco.3@gmail.com>
2017-02-07 01:34:52 +0000

    Update neil_b.js

--- routes/profiles/neil_b.js
+++ routes/profiles/neil_b.js
@@ -10,11 +10,7 @@ var blurb   = "Hi, my name is Neil (other Neil) and I am a student at West Colle
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-<<<<<<< HEAD
-  res.render('profiles/neil_b.jade', {
-=======
   res.render('profiles/neil_b.pug', {
->>>>>>> 614783ed12ae18ccb6981ae289c5e5b78e7ddd03
     name    : name,
     twitter : twitter,
     blurb   : blurb,
